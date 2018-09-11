259386b1685f8129e10aa87bd1069e707b6d8e0d
shinp <shinpeiyoshihara@gmail.com>
2018-09-12 01:19:07 +0900

    reClass

---
+++ classes/Arm.ts
@@ -0,0 +1,14 @@
+import math3d = require('math3d');
+import { Relation } from './Relation';
+
+export class Arm extends Relation{
+
+  constructor(prop:Object){
+
+
+    super(prop);
+  }
+
+  
+
+}
